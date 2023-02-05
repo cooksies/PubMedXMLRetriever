2d3bebbead1702c1163605f5b5cd3861c73651de
yegax <yegax.digital@gmail.com>
2023-02-05 16:01:26 -0500

    Initial Commit
    Changed date formatting method

--- Parse.java
+++ Parse.java
@@ -32,25 +32,25 @@ public class Parse {
 
     // Get the elements with the tag name "ArticleTitle"
     NodeList articleTitleNodes = doc.getElementsByTagName("ArticleTitle");
-    //NodeList authorLastNameNodes = doc.getElementsByTagName("LastName");
-    //NodeList authorInitialsNodes = doc.getElementsByTagName("Initials");
+    // NodeList authorLastNameNodes = doc.getElementsByTagName("LastName");
+    // NodeList authorInitialsNodes = doc.getElementsByTagName("Initials");
     NodeList affiliationNodes = doc.getElementsByTagName("Affiliation");
 
-    NodeList dayNodes = doc.getElementsByTagName("Day");
+    NodeList dayNodes = doc.getElementsByTagName("DateCreated");
+    
     NodeList monthNodes = doc.getElementsByTagName("Month");
     NodeList yearNodes = doc.getElementsByTagName("Year");
     
     for (int i = 0; i < articleTitleNodes.getLength(); i++) { 
         Element articleTitleElement = (Element) articleTitleNodes.item(i);
-        //Element authorLastNameElement = (Element)authorLastNameNodes.item(i);
-        //Element authorInitialsElement = (Element)authorInitialsNodes.item(i);
+        // Element authorLastNameElement = (Element)authorLastNameNodes.item(i);
+        // Element authorInitialsElement = (Element)authorInitialsNodes.item(i);
         Element affiliationElement  = (Element)affiliationNodes.item(i);
 
-    Element dayElement = (Element) dayNodes.item(i);
-    Element monthElement =(Element) monthNodes.item(i);
-    Element yearElement = (Element) yearNodes.item(i);
+        Element dayElement = (Element) dayNodes.item(i);
 
-        //DateReformatter dateReformat = new DateReformatter();
+        Element monthElement =(Element) monthNodes.item(i);
+        Element yearElement = (Element) yearNodes.item(i);
 
         /* Node dCom = doc.getElementsByTagName("DateCompleted").item(i);
         NodeList dateCompleteNodes = dCom.getChildNodes(); //i think this places me in a loop
@@ -59,26 +59,28 @@ public class Parse {
             //System.out.println(child.getNodeName() + ": " + child.getTextContent());
           } *///this doesn't work
 
-        //print the text content of each element
+
+        // print the text content of each element
         System.out.println("\n" +"Article Title: " + articleTitleElement.getTextContent() + "\n" +
         "Article Affiliation: " + affiliationElement.getTextContent() + "\n" +
-        //"Author Last Name: " + authorLastNameElement.getTextContent() + "\n" +
-        //"Author Initials: " + authorInitialsElement.getTextContent()); /* + "\n" +
+        // "Author Last Name: " + authorLastNameElement.getTextContent() + "\n" +
+        // "Author Initials: " + authorInitialsElement.getTextContent()  + "\n" +
 
-        " " + dayElement.getTextContent()  +
-        "/" + monthElement.getTextContent() +
-        "/" + yearElement.getTextContent());
 
-        // "Date Completed: " + dateReformat.getDate()); 
+        "Date Created: " + dayElement.getTextContent()
+        );
 
         //Save the title in the arrayList
         Titles.add(articleTitleElement.getTextContent());
         Affiliations.add(articleTitleElement.getTextContent());
-        //LastNames.add(authorLastNameElement.getTextContent());
-        //Initials.add(authorInitialsElement.getTextContent());
-        //dateCompleted.add(dateReformat.getDate());
+        // LastNames.add(authorLastNameElement.getTextContent());
+        // Initials.add(authorInitialsElement.getTextContent());
+        // dateCompleted.add(dateReformat.getDate());
     }
     }
 
+    public ArrayList<String> getTitles() {
+        return Titles;
+    }
 
 }
